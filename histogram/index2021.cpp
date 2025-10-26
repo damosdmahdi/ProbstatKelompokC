49e2e32e05f0b013efb01a6da840590b65083d1b
damosdmahdi <godrihidaayt2@gmail.com>
2025-10-26 16:46:00 +0700

    tambah hitogram dengan line made with py

--- histogram/index2021.cpp
+++ histogram/index2021.cpp
@@ -3,12 +3,10 @@
 #include <vector>
 #include <string>
 
-
 namespace plt = matplotlibcpp;
 
 int main() {
-
-    std::vector<double> frekuensi = {2.0, 4.0, 3.0, 1.0, 2.0};
+    std::vector<double> frekuensi = {5.0, 3.0, 2.0, 1.0, 1.0};
 
     std::vector<double> x_posisi = {0, 1, 2, 3, 4};
 
@@ -16,8 +14,7 @@ int main() {
         "0-100", "100-200", "200-300", "300-400", "400-500"
     };
 
-
-    plt::figure_size(1000, 700); 
+    plt::figure_size(1000, 700);
 
     plt::bar(x_posisi, frekuensi);
 
@@ -25,12 +22,19 @@ int main() {
     plt::ylabel("Frekuensi (Jumlah Bulan)");
     plt::xlabel("Curah Hujan (mm)");
 
-    std::vector<double> y_ticks = {0, 1, 2, 3, 4};
+    plt::xticks(x_posisi, x_labels);
+
+    std::vector<double> y_ticks;
+    for (int i = 0; i <= 6; ++i) {
+        y_ticks.push_back(static_cast<double>(i));
+    }
     plt::yticks(y_ticks);
 
     plt::grid(true);
 
-    plt::save("histogram_persis_2021.png");
+    plt::tight_layout();
+
+    plt::save("histogram_tabel_2021.png");
     plt::show();
 
     return 0;
